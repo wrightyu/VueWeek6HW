782c7b7bc5094a4becc0d5ccaf682bdd98c16824
WrightYu <wright820328@gmail.com>
2023-02-24 01:19:51 +0800

    deploy

---
+++ assets/HomeView-13c10aa5.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as s}from"./index-01b5661e.js";const t={components:{}},c={class:"home"};function n(r,a,_,p,i,m){return o(),s("div",c," 這是首頁 ")}const f=e(t,[["render",n]]);export{f as default};
